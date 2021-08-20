ac9a0b32fa3a514e090f63a23c864c812b594a54
maroun-baydoun <maroun.baydoun@gmail.com>
2021-08-20 16:08:53 +0200

    Clear files

--- components/index.js
+++
@@ -1,4 +0,0 @@
-import { TinyContainer } from "./container.js";
-import { TinyItem } from "./item.js";
-
-export { TinyContainer, TinyItem };
--- jest-puppeteer.config.cjs
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  server: {
-    command: "npm run start",
-    port: 8888,
-    usedPortAction: "kill",
-    debug: false,
-  },
-};
