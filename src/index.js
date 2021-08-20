1767921ec8b7420e46022645c8c1b7dfd00a53c6
maroun-baydoun <maroun.baydoun@gmail.com>
2021-08-20 18:11:54 +0200

    Add content

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
+import "tiny-flex";
 import "@fontsource/cutive-mono";
 import "@fontsource/roboto/latin-400.css";
 import "@fontsource/roboto/latin-500.css";
 
 import "./scss/reset.scss";
 import "./scss/index.scss";
+
+import "./prismjs";
